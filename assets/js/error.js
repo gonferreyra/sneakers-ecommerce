8f87baa7158f959847708086d047c0c66c118a39
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-08-11 11:13:40 -0300

    fix error message

--- assets/js/error.js
+++ assets/js/error.js
@@ -5,7 +5,7 @@ import { sneakers } from "./db.js";
 export function showError(message) {
     cleanErrorHTML()
     const alerta = document.createElement('p')
-    alerta.style = 'background:red;color:white;width:510px;margin:1rem auto;padding:1rem;text-align:center;border-radius:5px;'
+    alerta.style = 'background:red;color:white;width:85%;max-width:510px;margin:1rem auto;padding:1rem;text-align:center;border-radius:5px;'
     alerta.innerText = message
     v.errorContainer.appendChild(alerta)
     setTimeout(() => {
@@ -23,7 +23,7 @@ export function cleanErrorHTML() {
 export function showErrorLogin(message) {
     cleanErrorHTML()
     const alerta = document.createElement('p')
-    alerta.style = 'background:red;color:white;width:510px;margin:1rem auto;padding:1rem;text-align:center;border-radius:5px;'
+    alerta.style = 'background:red;color:white;width:85%;max-width:510px;margin:1rem auto;padding:1rem;text-align:center;border-radius:5px;'
     alerta.innerText = message
     v.formError.appendChild(alerta)
     setTimeout(() => {
